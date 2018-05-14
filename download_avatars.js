eff5e8c60ebeae7ed21e919f1e46a59f9fdbd22c
Anisa Thomas <anisathomas99@gmail.com>
2018-05-14 22:17:25 +0000

    Added Authorization Header with Token in js file

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,39 @@
 var request = require('request');
-
+var secrets = require('./secrets')
+console.log("Secrets:", secrets);
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  // request(url, function(err, res, body) {
+  //   cb(err, body);
+  // });
+  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization' : 'token' + secrets.GITHUB_TOKEN
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
+
+
+
+
+
+
+
+
