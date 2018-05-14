a75c4788e4b5d9340274e61aafbd01de75be626e
Anisa Thomas <anisathomas99@gmail.com>
2018-05-14 23:10:24 +0000

    Parsed the JSON string and added a for loop to console.log the value for each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -1,15 +1,11 @@
 var request = require('request');
-var secrets = require('./secrets')
-console.log("Secrets:", secrets);
+var secrets = require('./secrets');
+//console.log("Secrets:", secrets);
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  // request(url, function(err, res, body) {
-  //   cb(err, body);
-  // });
-  // ...
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -19,20 +15,34 @@ function getRepoContributors(repoOwner, repoName, cb) {
   };
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    var data = JSON.parse(body);
+
+    cb(err, data);
   });
 }
 
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+  for(var i = 0; i < result.length; i++){
+      console.log(result[i].avatar_url);
+
+    }
+
   console.log("Errors:", err);
-  console.log("Result:", result);
+  //console.log("Result:", result);
 });
 
 
 
 
 
+
+
+// Step 7: Print each avatar_url
+// Your next and final step in this exercise should be to change your
+// getRepoContributors function to parse the JSON string into an object and pass this object (an array of contributor objects) to the cb function.
+
+// You will also need to modify the callback function to iterate over the results and (for now) console.log the value for each avatar_url in the collection:
 
 
 
